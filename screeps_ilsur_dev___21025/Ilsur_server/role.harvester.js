cd48448c6acf6b93f35b72caef44f777d20d5718
Vlaf <razin36986@gmail.com>
2020-04-17 01:51:00 +0500

    commit

--- screeps_ilsur_dev___21025/Ilsur_server/role.harvester.js
+++ screeps_ilsur_dev___21025/Ilsur_server/role.harvester.js
@@ -15,6 +15,7 @@ var roleHarvester = {
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
                 });
+                targets.reverse()
                 if(targets.length > 0) {
                     if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
